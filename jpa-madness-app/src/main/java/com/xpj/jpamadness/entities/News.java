390ccadcdf87a92017a13ac711052d2b4b07f69c
Piotr Jeziorski <piotrek.jeziorski@gmail.com>
2024-12-25 12:01:43 +0100

    entities base

---
+++ jpa-madness-app/src/main/java/com/xpj/jpamadness/entities/News.java
@@ -0,0 +1,28 @@
+package com.xpj.jpamadness.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.LAZY) // cascade in order to save tags if they do not exist
+    @JoinColumn(name = "news_id")
+    private List<NewsComment> comments;
+
+}
